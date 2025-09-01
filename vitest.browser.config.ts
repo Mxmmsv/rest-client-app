ca5650a0d4ba705f53f2c2bae132a82715fdf5fc
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-01 20:20:14 +0300

    chore: install vitest, add scripts, add vitest.browser.config.ts

---
+++ vitest.browser.config.ts
@@ -0,0 +1,37 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      // https://vitest.dev/guide/browser/playwright
+      instances: [
+        {
+          browser: 'chromium',
+        },
+      ],
+    },
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/main.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
+});
