df59d71c6eeab2a17f23f28bc43c67b8da12633a
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-01 19:17:22 +0300

    refactor: use format:fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
+import './globals.css';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
-import "./globals.css";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "REST client",
-  description: "REST client app for final task in RSSchool",
+  title: 'REST client',
+  description: 'REST client app for final task in RSSchool',
 };
 
 export default function RootLayout({
