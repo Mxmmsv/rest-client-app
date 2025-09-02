1eac567caf49f7fb8cc03b8b7b48f5c518dfc4ae
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-03 02:40:21 +0300

    feat: add button controls state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
 import { Button } from 'antd';
 
+import { useAppDispatch, useAppSelector } from '@/lib/store/hooks';
+import { increment, decrement } from '@/lib/store/slice/counterSlice';
+
 export default function Home() {
+  const value = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
+    <div className="align-center flex gap-1">
+      <Button type="primary" disabled={value <= 0} onClick={() => dispatch(decrement())}>
+        -
+      </Button>
+      <span>{value}</span>
+      <Button type="primary" disabled={value >= 10} onClick={() => dispatch(increment())}>
+        +
+      </Button>
     </div>
   );
 }
