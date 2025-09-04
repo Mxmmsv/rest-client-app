1d5cd559a561d3fd91ca6d9b8ef01977195d417d
ek-ole <ek_34@mail.ru>
2025-09-04 14:25:03 +0300

    feat: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from 'antd';
 
+import Header from './components/header';
+
 export default function Home() {
   return (
     <div className="App">
+      <Header></Header>
       <Button type="primary">Button</Button>
     </div>
   );
