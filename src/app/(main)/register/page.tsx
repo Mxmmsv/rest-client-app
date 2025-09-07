83f556b6b43ce164ae3b850578c321147e82e018
Mxmmsv <61119540+Mxmmsv@users.noreply.github.com>
2025-09-07 15:34:34 +0300

    feat: implement sign in and sign up,  add firebase provider (#34)
    * chore: install redux toolkit, react-redux
    
    * feat: implement RTK
    
    * refactor: add provider, remove comment
    
    * feat: add basic slice
    
    * refactor: remove providers
    
    * feat: add button controls state
    
    * chore: install antd/icons
    
    * feat: implement antd/icons to button controls state
    
    * chore: install clsx and tailwind-merge
    
    * fix: add alias resolve for tests
    
    * test: add tests
    
    * docs: move pr template to another dir
    
    * refactor: change default layout component to antDesign components
    
    * chore: add test:coverage script
    
    * feat: create counter.ts selector, change declaration selector in page.tsx
    
    * chore: install firebase and react-firebase-hooks
    
    * feat: add firebase config and func to signIn, signUp and logOut
    
    * feat: add signIn and signUp components, update auth.ts
    
    * refactor: rename type
    
    * feat: implement toast
    
    * chore: install antd
    
    * refactor: move to another dir
    
    * refactor: change to hook
    
    * feat: add header.tsx
    
    * feat: create route for signIn and signUp
    
    * feat: add header to root layout
    
    * feat: add validation form input
    
    * feat: add redirect to main page after login
    
    * chore: add vitest ui
    
    * chore: remove vitest --browser
    
    * chore: npm audit after merge
    
    * refactor: change nesting for tags
    
    * refactor: move components to another dir
    
    * refactor: remove unused page and test
    
    * refactor: change level for title component

---
+++ src/app/(main)/register/page.tsx
@@ -0,0 +1,5 @@
+import SignUp from '@/components/singUp';
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
---
+++ src/components/singUp.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { Button, Flex, Form, Input, Spin, Typography } from 'antd';
+import useNotification from 'antd/es/notification/useNotification';
+import { redirect } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from '@/lib/auth/firebase.config';
+import { UserData, useAuth } from '@/lib/auth/useAuth';
+
+type FieldType = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+export default function SignUp() {
+  const [user, loading, error] = useAuthState(auth);
+  const { registerWithEmailAndPassword } = useAuth();
+  const [api, contextHolder] = useNotification();
+
+  if (loading) {
+    return (
+      <Flex justify="center" align="center">
+        <Spin />
+      </Flex>
+    );
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (user) {
+    redirect('/');
+  }
+
+  const onFinish = async ({ email, password, name }: UserData) => {
+    await registerWithEmailAndPassword({ email, password, name, api });
+  };
+
+  return (
+    <Flex justify="center" align="center" vertical>
+      {contextHolder}
+      <Typography.Title>Welcome!</Typography.Title>
+      <Form
+        name="signUp"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'Please enter a valid email!' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[
+            { required: true, message: 'Please input your password!' },
+            {
+              pattern: /^(?=.*\p{L})(?=.*\d)(?=.*[^\p{L}\d]).{8,}$/u,
+              message:
+                'Password must be at least 8 characters long and contain a letter, a number, and a special character.',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Name"
+          name="name"
+          rules={[
+            { required: true, message: 'Please input your name!' },
+            { min: 2, message: 'Name must be at least 2 characters long.' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Flex>
+  );
+}
