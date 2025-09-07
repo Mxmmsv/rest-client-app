48f040527e450002106b26de204cf8122e14ad99
ek-ole <ek_34@mail.ru>
2025-09-08 00:50:57 +0300

    test: add test for logout button

--- src/components/header/__tests__/header.test.tsx
+++ src/components/header/__tests__/header.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 
-import { createTestStore } from '@/app/__tests__/mock-redux';
+import { mockUser } from '@/components/__mock__/firebaseUser.mock';
+import { useAuth } from '@/lib/auth/useAuth';
 
 import Header from '../header';
 
@@ -16,49 +17,69 @@ vi.mock('react', async () => {
   };
 });
 
+vi.mock('react-firebase-hooks/auth', () => ({
+  useAuthState: vi.fn(),
+}));
+
+vi.mock('@/lib/auth/useAuth', () => ({
+  useAuth: vi.fn(),
+}));
+
+vi.mock('antd/es/notification/useNotification', () => ({
+  __esModule: true,
+  default: () => [vi.fn(), <div key="ctx">NotificationCtx</div>],
+}));
+
+const mockedUseAuthState = vi.mocked(useAuthState);
+const mockedUseAuth = vi.mocked(useAuth);
+
 describe('Header component', () => {
   const setStickyMock = vi.fn();
+  const mockLogout = vi.fn();
 
   beforeEach(() => {
     vi.mocked(useState).mockImplementation(() => [false, setStickyMock]);
     vi.mocked(useEffect).mockImplementation((fn) => fn());
+    mockedUseAuth.mockReturnValue({
+      logInWithEmailAndPassword: vi.fn(),
+      registerWithEmailAndPassword: vi.fn(),
+      logout: mockLogout,
+    });
+
+    mockedUseAuthState.mockReturnValue([null, false, undefined]);
   });
 
-  it('should shows Sign In an d sign Up when user is not authenticated', () => {
-    const testStore = createTestStore({ auth: { isAuthenticated: false } });
-    render(
-      <Provider store={testStore}>
-        <Header />
-      </Provider>
-    );
+  it('should shows Login and Register when user is not authenticated', () => {
+    render(<Header />);
 
     expect(screen.getByText(/login/i)).toBeInTheDocument();
     expect(screen.getByText(/register/i)).toBeInTheDocument();
     expect(screen.queryByText(/logout/i)).not.toBeInTheDocument();
   });
 
-  it('should shows Sign Out when user is authenticated', () => {
-    const testStore = createTestStore({ auth: { isAuthenticated: true } });
-    render(
-      <Provider store={testStore}>
-        <Header />
-      </Provider>
-    );
+  it('should shows Logout when user is authenticated', () => {
+    mockedUseAuthState.mockReturnValue([mockUser, false, undefined]);
+
+    render(<Header />);
 
     expect(screen.getByRole('button', { name: /logout/i })).toBeInTheDocument();
     expect(screen.queryByText(/login/i)).not.toBeInTheDocument();
     expect(screen.queryByText(/register/i)).not.toBeInTheDocument();
   });
 
+  it('should call logout function when Logout button is clicked', () => {
+    mockedUseAuthState.mockReturnValue([mockUser, false, undefined]);
+
+    render(<Header />);
+    const signOutButton = screen.getByRole('button', { name: /logout/i });
+    fireEvent.click(signOutButton);
+
+    expect(mockLogout).toHaveBeenCalled();
+  });
+
   it('should render logo with small size when isSticky is true', () => {
     vi.mocked(useState).mockImplementation(() => [true, setStickyMock]);
-
-    const testStore = createTestStore({ auth: { isAuthenticated: false } });
-    render(
-      <Provider store={testStore}>
-        <Header />
-      </Provider>
-    );
+    render(<Header />);
 
     const logo = screen.getByAltText('REST Client Logo');
     expect(logo).toHaveAttribute('width', '40');
@@ -67,13 +88,7 @@ describe('Header component', () => {
 
   it('should render logo with large size when isSticky is false', () => {
     vi.mocked(useState).mockImplementation(() => [false, setStickyMock]);
-
-    const testStore = createTestStore({ auth: { isAuthenticated: false } });
-    render(
-      <Provider store={testStore}>
-        <Header />
-      </Provider>
-    );
+    render(<Header />);
 
     const logo = screen.getByAltText('REST Client Logo');
     expect(logo).toHaveAttribute('width', '55');
