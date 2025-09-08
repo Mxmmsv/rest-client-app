626e2962aa0ec4f261db4e5be6a5909d16ef5e60
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-09 02:17:51 +0300

    fix: fix path trouble for signIn and signUp

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,4 +1,4 @@
-import SignIn from '@/components/signIn';
+import SignIn from '@/components/main/signIn';
 
 export default function SignInPage() {
   return <SignIn />;
--- src/app/(main)/register/page.tsx
+++ src/app/(main)/register/page.tsx
@@ -1,4 +1,4 @@
-import SignUp from '@/components/signUp';
+import SignUp from '@/components/main/signUp';
 
 export default function SignUpPage() {
   return <SignUp />;
