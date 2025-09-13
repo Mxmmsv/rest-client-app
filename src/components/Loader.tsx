15f50aa7b17594058b763553128cafd86029b684
ek-ole <ek_34@mail.ru>
2025-09-13 23:20:56 +0300

    fix: fix lottie loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { Player } from '@lottiefiles/react-lottie-player';
 import { Flex } from 'antd';
+import Lottie from 'lottie-react';
+
+import animationData from '@/animations/loader.json';
 
 export default function Loader() {
   return (
     <Flex vertical justify="center" align="center" className="h-[80vh]">
-      <Player autoplay loop src="/loader.json" style={{ height: '300px', width: '300px' }} />
+      <Lottie animationData={animationData} style={{ height: '300px', width: '300px' }} />
     </Flex>
   );
 }
