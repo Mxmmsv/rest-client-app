94ae45b2bb5127515b529588dd21a86c0220ebf6
ek-ole <ek_34@mail.ru>
2025-09-14 00:44:20 +0300

    refucktor: change lottie to spiner

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { Flex } from 'antd';
-import dynamic from 'next/dynamic';
-
-const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
-import animationData from '@/animations/loader.json';
+import { Flex, Spin } from 'antd';
 
 export default function Loader() {
   return (
     <Flex vertical justify="center" align="center" className="h-[80vh]">
-      <Lottie animationData={animationData} style={{ height: '300px', width: '300px' }} />
+      <Spin />
     </Flex>
   );
 }
