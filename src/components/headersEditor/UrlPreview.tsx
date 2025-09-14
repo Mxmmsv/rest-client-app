00e69c2d4d1bf5d18866c409e8264595ad7b3293
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-09-14 09:43:28 +0200

    feat: implement url preview

---
+++ src/components/headersEditor/UrlPreview.tsx
@@ -0,0 +1,39 @@
+import { CopyOutlined } from '@ant-design/icons';
+import { Button, Col, Divider, Form, Input } from 'antd';
+import { Row } from 'antd/lib';
+import { useState, useEffect } from 'react';
+
+import { UrlPreviewProps } from '@/types/headersSectionTypes';
+
+export default function UrlPreview({ endpoint, headers }: Readonly<UrlPreviewProps>) {
+  const [fullUrl, setFullUrl] = useState<string>(endpoint);
+
+  useEffect(() => {
+    const headerParams = Object.entries(headers)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&');
+
+    const urlWithHeaders = headerParams ? `${endpoint}?${headerParams}` : endpoint;
+    setFullUrl(urlWithHeaders);
+  }, [endpoint, headers]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(fullUrl);
+  };
+
+  return (
+    <Form>
+      <Divider orientation="left">URL Preview</Divider>
+      <Form.Item name="url">
+        <Row wrap={false}>
+          <Col flex="auto">
+            <Input readOnly placeholder="URL preview" value={fullUrl} />
+          </Col>
+          <Col flex="none">
+            <Button type="text" icon={<CopyOutlined />} onClick={handleCopy} />
+          </Col>
+        </Row>
+      </Form.Item>
+    </Form>
+  );
+}
