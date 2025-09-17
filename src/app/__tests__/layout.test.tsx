7fa2612afc8f9d1768b22c4f9d098f24c6ded7b9
ek-ole <ek_34@mail.ru>
2025-09-18 00:55:05 +0300

    refactor: change font

--- src/app/__tests__/layout.test.tsx
+++ src/app/__tests__/layout.test.tsx
@@ -3,7 +3,7 @@ import { expect, test } from 'vitest';
 
 vi.mock('next/font/google', () => ({
   Press_Start_2P: () => ({ variable: 'mock-logo-font' }),
-  Quantico: () => ({ variable: 'mock-primary-font' }),
+  Exo_2: () => ({ variable: 'mock-primary-font' }),
 }));
 
 import RootLayout from '../layout';
