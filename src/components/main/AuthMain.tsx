ab7e9c4ee06d3c267d14f9fffc716b24f511f816
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-19 00:32:07 +0300

    refactor: update styles for authMain

--- src/components/main/AuthMain.tsx
+++ src/components/main/AuthMain.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Typography } from 'antd';
+import { Button, Card, Flex, Typography } from 'antd';
 import Link from 'next/link';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
@@ -13,18 +13,22 @@ export default function AuthMain() {
 
   return (
     <Flex vertical gap={10} justify="center" align="center" style={{ height: '100vh' }}>
-      <Title>{`Welcome back, ${user?.displayName || 'user'}`}</Title>
-      <Flex gap={10} justify="center" align="center">
-        <Link href="/rest-client">
-          <Button>Rest Client</Button>
-        </Link>
-        <Link href="/history">
-          <Button>History</Button>
-        </Link>
-      </Flex>
-      <Button type="primary" onClick={logout}>
-        Logout
-      </Button>
+      <Card>
+        <Title>{`Welcome back, ${user?.displayName || 'user'}`}</Title>
+        <Flex gap={10} justify="center" align="center">
+          <Link href="/rest-client">
+            <Button>Rest Client</Button>
+          </Link>
+          <Link href="/history">
+            <Button>History</Button>
+          </Link>
+        </Flex>
+        <Flex justify="flex-end">
+          <Button type="primary" onClick={logout}>
+            Logout
+          </Button>
+        </Flex>
+      </Card>
     </Flex>
   );
 }
