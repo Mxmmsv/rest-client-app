77e7bbad046951adbaf9409d238cc2990524ed9f
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-19 15:34:38 +0300

    style: update styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,25 @@
-import { Button, Result } from 'antd';
+import { Button, Result, Layout } from 'antd';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
+const { Content } = Layout;
+
 function NotFoundPage() {
   const t = useTranslations('NotFoundPage');
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle={t('sorry')}
-      extra={
-        <Link href="/" passHref>
-          <Button type="primary">{t('button-back')}</Button>
-        </Link>
-      }
-    />
+    <Content>
+      <Result
+        status="404"
+        title="404"
+        subTitle={t('sorry')}
+        extra={
+          <Link href="/" passHref>
+            <Button type="primary">{t('button-back')}</Button>
+          </Link>
+        }
+      />
+    </Content>
   );
 }
 
--- src/components/main/SignIn.tsx
+++ src/components/main/SignIn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Flex, Form, Input } from 'antd';
+import { Button, Flex, Form, Input, Layout } from 'antd';
 import { redirect } from 'next/navigation';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
@@ -13,6 +13,8 @@ type FieldType = {
   email: string;
   password: string;
 };
+
+const { Content } = Layout;
 
 export default function SignIn() {
   const [user, loading, error] = useAuthState(auth);
@@ -35,30 +37,32 @@ export default function SignIn() {
   }
 
   return (
-    <Flex justify="center" align="center" style={{ height: '100vh' }}>
-      <Form name="signIn" labelCol={{ span: 8 }} onFinish={onFinish}>
-        <Form.Item<FieldType>
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: 'Please input your email!' }]}
-        >
-          <Input />
-        </Form.Item>
+    <Content>
+      <Flex justify="center" align="center" style={{ height: '75vh' }}>
+        <Form name="signIn" labelCol={{ span: 8 }} onFinish={onFinish}>
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item label={null} className="flex justify-center">
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </Flex>
+          <Form.Item label={null} className="flex justify-center">
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Flex>
+    </Content>
   );
 }
