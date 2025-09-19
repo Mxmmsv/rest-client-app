19525dc971bc2aa8ea51fcf1e5789064734124d7
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-19 17:36:55 +0300

    style: update styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,21 @@
-import { Button, Result, Layout } from 'antd';
+import { Button, Result } from 'antd';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
-const { Content } = Layout;
-
 function NotFoundPage() {
   const t = useTranslations('NotFoundPage');
   return (
-    <Content>
-      <Result
-        status="404"
-        title="404"
-        subTitle={t('sorry')}
-        extra={
-          <Link href="/" passHref>
-            <Button type="primary">{t('button-back')}</Button>
-          </Link>
-        }
-      />
-    </Content>
+    <Result
+      status="404"
+      title="404"
+      subTitle={t('sorry')}
+      extra={
+        <Link href="/" passHref>
+          <Button type="primary">{t('button-back')}</Button>
+        </Link>
+      }
+    />
   );
 }
 
