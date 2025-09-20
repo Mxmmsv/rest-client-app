31fc0c339ab15afd2e85f95cfe66e3f6f55306cd
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-20 15:24:37 +0300

    refactor: update lazy-loaded

--- src/app/[locale]/(main)/rest-client/page.tsx
+++ src/app/[locale]/(main)/rest-client/page.tsx
@@ -1,22 +1,22 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { redirect } from 'next/navigation';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import Loader from '@/components/Loader';
-import RestClient from '@/components/restClient/RestClient';
 import { auth } from '@/lib/auth/firebase.config';
+
+const RestClient = dynamic(() => import('@/components/restClient/RestClient'), {
+  loading: () => <Loader />,
+});
 
 export default function RestClientPage() {
   const [user, loading] = useAuthState(auth);
 
-  if (loading) {
-    return <Loader />;
-  }
+  if (loading) return <Loader />;
 
-  if (user) {
-    return <RestClient />;
-  }
+  if (!user) return redirect('/');
 
-  return redirect('/');
+  return <RestClient />;
 }
