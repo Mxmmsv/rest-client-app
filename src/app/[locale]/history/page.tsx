23d99f9d34de7465a9a1fc245362c13cc7359a2a
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-09-21 23:35:13 +0200

    feat: add page.tsx

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,4 +1,10 @@
-import HistoryPageContent from '@/components/historyPage/HistoryPageContent';
+import dynamic from 'next/dynamic';
+
+import Loader from '@/components/Loader';
+
+const HistoryPageContent = dynamic(() => import('@/components/history/HistoryPageContent'), {
+  loading: () => <Loader />,
+});
 
 export default function HistoryPage() {
   return <HistoryPageContent />;
