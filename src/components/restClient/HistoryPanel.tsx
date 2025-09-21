18abe395c94ab24e08eb04a74959be268f7fa25f
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-09-21 14:27:42 +0200

    feat: add link to history page in HistoryPanel component

--- src/components/restClient/HistoryPanel.tsx
+++ src/components/restClient/HistoryPanel.tsx
@@ -1,5 +1,21 @@
+import { Flex, Typography } from 'antd';
 import Title from 'antd/es/typography/Title';
 
+const { Text, Link } = Typography;
+
 export default function HistoryPanel() {
-  return <Title level={3}>History & Analytics</Title>;
+  return (
+    <>
+      <Flex vertical gap="small" justify="center" align="center">
+        <Title level={3}>History & Analytics</Title>
+        <Text style={{ padding: '16px' }}>
+          To view the request history and analytics, click{' '}
+          <Link underline href="/history">
+            here
+          </Link>
+          .
+        </Text>
+      </Flex>
+    </>
+  );
 }
